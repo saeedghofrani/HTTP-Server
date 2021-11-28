83fcdf5109d9d6c310b3a1b8f40db4e72fe69080
saeed <sa.ghofraniivari@gmail.com>
2021-11-29 01:09:07 +0330

    debug V1.0

--- 6-Https template/main.js
+++ 6-Https template/main.js
@@ -13,16 +13,16 @@ let server = http.createServer((req, res) => {
             case '/team':
                 res.end(pageData.userTable);
                 break;
-            case 'css/userTableStyle.css':
+            case '/css/userTableStyle.css':
                 res.end(pageData.userTableCss);
                 break;
-            case 'js/CRUD.js':
+            case '/js/CRUD.js':
                 res.end(pageData.userTableCRUD);
                 break;
-            case 'js/callData.js':
+            case '/js/callData.js':
                 res.end(pageData.userTableCallData);
                 break;
-            case 'js/GUI.js':
+            case '/js/GUI.js':
                 res.end(pageData.userTableGUI);
                 break;
             //user put nothing in url//
