140dab4d106d7afbbf1a5f7d9bb1dbec6b6e9d08
saeed <sa.ghofraniivari@gmail.com>
2021-11-30 01:23:15 +0330

    replace const

--- Modules/readPage.js
+++ Modules/readPage.js
@@ -1,7 +1,7 @@
 "use strict";
 const fs = require('fs');
 function readPage(path) {
-    const data = fs.readFileSync(path, {encoding: 'utf-8'});
+    const data = fs.readFileSync(path, { encoding: 'utf-8' });
     return data;
 }
 module.exports = readPage;
