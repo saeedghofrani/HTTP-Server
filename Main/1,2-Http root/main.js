e5ff570d2a53c0508f8a6487b6a269214af633c5
saeed <sa.ghofraniivari@gmail.com>
2021-12-02 16:08:45 +0330

    debug root

--- Main/1,2-Http root/main.js
+++ Main/1,2-Http root/main.js
@@ -3,6 +3,7 @@ const http = require('http');
 const url = require('url');
 const page = require('../../Modules/pageHeader.js');
 const {root} = require('../../Modules/addressDictionary.js');
+
 //create server//
 let server = http.createServer((req, res) => {
     //log url request//
